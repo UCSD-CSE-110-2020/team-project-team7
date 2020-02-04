f83ae0cf87d4caf1fdd75183879e8f91880c5818
Amrit-S <aksingh@ucsd.edu>
2020-02-03 20:58:10 -0800

    Baseline of App

---
+++ WalkWalkRevolution/app/src/main/java/com/example/walkwalkrevolution/RoutesList.java
@@ -0,0 +1,14 @@
+package com.example.walkwalkrevolution;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoutesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes_list);
+    }
+}
