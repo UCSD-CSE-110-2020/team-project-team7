764634cd3cf92bbd4aa6271a98867412c39857c0
Yoshi Russell <yoshiichirorussell@gmail.com>
2020-02-07 15:11:53 -0800

    updated file path in run_tests_on_firebase

--- WalkWalkRevolution/run_tests_on_firebase_test_lab.sh
+++ WalkWalkRevolution/run_tests_on_firebase_test_lab.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 echo "Building debug and release APKs..."
-./WalkWalkRevolution/gradlew :app:assembleDebug :app:assembleDebugAndroidTest
+./gradlew :app:assembleDebug :app:assembleDebugAndroidTest
 
 echo "Downloading google-cloud-sdk..."
 wget --quiet --output-document=/tmp/google-cloud-sdk.tar.gz https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.tar.gz
@@ -21,6 +21,6 @@ gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
 echo "Running Android Tests with Firebase Test Lab..."
 gcloud firebase test android run \
    --type instrumentation \
-   --app ./app/build/outputs/apk/debug/app-debug.apk \
-   --test ./app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
+   --app ./WalkWalkRevolution/app/build/outputs/apk/debug/app-debug.apk \
+   --test ./WalkWalkRevolution/app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
 
