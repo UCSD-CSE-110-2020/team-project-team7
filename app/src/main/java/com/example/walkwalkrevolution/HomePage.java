ae0e457b3235a9bc47f0addf406c7c0979685aad
Calvin Nguy <cnguy@ucsd.edu>
2020-02-08 15:11:14 -0800

    Integrated Lab4 with Homepage

--- app/src/main/java/com/example/walkwalkrevolution/HomePage.java
+++ app/src/main/java/com/example/walkwalkrevolution/HomePage.java
@@ -2,13 +2,49 @@ package com.example.walkwalkrevolution;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.walkwalkrevolution.fitness.FitnessService;
+import com.example.walkwalkrevolution.fitness.FitnessServiceFactory;
+import com.example.walkwalkrevolution.fitness.GoogleFitAdapter;
 
 public class HomePage extends AppCompatActivity {
+
+    private String fitnessServiceKey = "GOOGLE_FIT";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Get id of temp button
+        Button btnGoToSteps = findViewById(R.id.tempButt);
+        btnGoToSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchStepCountActivity();
+            }
+        });
+
+        // Setup fitness service
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(StepCountActivity stepCountActivity) {
+                return new GoogleFitAdapter(stepCountActivity);
+            }
+        });
+    }
+
+    public void launchStepCountActivity() {
+        Intent intent = new Intent(this, StepCountActivity.class);
+        intent.putExtra(StepCountActivity.FITNESS_SERVICE_KEY, fitnessServiceKey);
+        startActivity(intent);
+    }
+
+    public void setFitnessServiceKey(String fitnessServiceKey) {
+        this.fitnessServiceKey = fitnessServiceKey;
     }
 }
---
+++ app/src/main/java/com/example/walkwalkrevolution/StepCountActivity.java
@@ -0,0 +1,64 @@
+package com.example.walkwalkrevolution;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.walkwalkrevolution.fitness.FitnessService;
+import com.example.walkwalkrevolution.fitness.FitnessServiceFactory;
+
+public class StepCountActivity extends AppCompatActivity {
+
+    public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
+
+    private static final String TAG = "StepCountActivity";
+
+    private TextView textSteps;
+    private FitnessService fitnessService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_count);
+        textSteps = findViewById(R.id.textSteps);
+
+        String fitnessServiceKey = getIntent().getStringExtra(FITNESS_SERVICE_KEY);
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
+
+        Button btnUpdateSteps = findViewById(R.id.buttonUpdateSteps);
+        btnUpdateSteps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fitnessService.updateStepCount();
+            }
+        });
+
+        fitnessService.setup();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+//       If authentication was required during google fit setup, this will be called after the user authenticates
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == fitnessService.getRequestCode()) {
+                fitnessService.updateStepCount();
+            }
+        } else {
+            Log.e(TAG, "ERROR, google fit result code: " + resultCode);
+        }
+    }
+
+    public void setStepCount(long stepCount) {
+        textSteps.setText(String.valueOf(stepCount));
+    }
+
+}
