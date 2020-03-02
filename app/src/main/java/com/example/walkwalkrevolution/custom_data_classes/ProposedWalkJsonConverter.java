0d27cce30d8376140e29d554d9cf1bc75ef65fa8
Titan-Ngo <ttngo@ucsd.edu>
2020-03-01 17:56:46 -0800

    wrote json formatting classes, fixed nullptr exceptions, fixed intent errors

---
+++ app/src/main/java/com/example/walkwalkrevolution/custom_data_classes/ProposedWalkJsonConverter.java
@@ -0,0 +1,41 @@
+package com.example.walkwalkrevolution.custom_data_classes;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+/**
+ * This class provides static public methods for converting Proposed Walk objects to and from jsons.
+ */
+public class ProposedWalkJsonConverter {
+
+    public static final String TAG = "ProposedWalkJsonConverter";
+
+    /**
+     * Takes in a ProposedWalk and returns its Json string.
+     */
+    public static String convertWalkToJson(ProposedWalk proposedWalk) {
+        Gson gson = new Gson();
+
+        // Convert Proposed Walk to JSON string
+        String proposedWalkStr = gson.toJson(proposedWalk);
+
+        Log.d(TAG, "Converted a ProposedWalk with the name: " + proposedWalk.name + " to Json str");
+
+        return proposedWalkStr;
+    }
+
+    /**
+     * Takes in a ProposedWalk that was converted into a Json string and returns a ProposedWalk.
+     */
+    public static ProposedWalk convertJsonToWalk(String proposedWalkStr) {
+        Gson gson = new Gson();
+
+        ProposedWalk proposedWalk = gson.fromJson(proposedWalkStr, ProposedWalk.class);
+
+        Log.d(TAG, "Converted Json string to a ProposedWalk with the name: " + proposedWalk.name);
+
+        return proposedWalk;
+    }
+
+}
