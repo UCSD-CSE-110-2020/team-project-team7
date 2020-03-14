d826379a4e2ac9dfe8915e7482608f80c82ea0af
Amrit-S <aksingh@ucsd.edu>
2020-03-13 23:43:16 -0700

    Testing is done

---
+++ app/src/androidTest/java/com/example/walkwalkrevolution/CancelInProgressTeamInvite.java
@@ -0,0 +1,50 @@
+package com.example.walkwalkrevolution;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CancelInProgressTeamInvite {
+
+    Intent intent;
+    private static final String TEST_SERVICE = "TEST_SERVICE";
+
+    @Rule
+    public ActivityTestRule<HomePage> mActivityTestRule = new ActivityTestRule<>(HomePage.class,true,false);
+
+    @Before
+    public void setUp(){
+        //clearSharedPrefs();
+        intent = new Intent( getInstrumentation().getTargetContext(), HomePage.class);
+        intent.putExtra(HomePage.FITNESS_SERVICE_KEY, TEST_SERVICE);
+        intent.putExtra("testService", true);
+        intent.putExtra("creator", true);
+        intent.putExtra("testStep", true);
+    }
+
+    private void clearSharedPrefs() {
+        Context targetContext = getInstrumentation().getTargetContext();
+        SharedPreferences sharedPreferences = targetContext.
+                getSharedPreferences("MyPrefsFile", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putBoolean("my_first_time", true).commit();
+    }
+
+    @Test
+    public void homePageOriginal() {
+        mActivityTestRule.launchActivity(intent);
+    }
+}
